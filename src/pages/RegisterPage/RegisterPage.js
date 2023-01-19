b5d27bf16dc7f1e46cd819f75964bdffba150a80
DFCruzz <diegofcruzz@gmail.com>
2023-01-19 15:11:28 -0300

    sign up function added

--- src/pages/RegisterPage/RegisterPage.js
+++ src/pages/RegisterPage/RegisterPage.js
@@ -1,27 +1,130 @@
 import StyledButton from "../../components/StyledButton"
 import StyledForm from "../../components/StyledForm"
 import { RegisterWrapper } from "./styles"
+import { useState } from "react"
+import axios from "axios"
+import { Link, useNavigate } from "react-router-dom"
+import env from "react-dotenv"
+import { toast } from "react-toastify"
 
 
 
-const RegisterPage = () => {
+
+const RegisterPage = ({ }) => {
+
+    const navigate = useNavigate()
+    const [user, setUser] = useState({
+        name: "",
+        email: "",
+        password: "",
+        confirmPassword: ""
+    })
+    
+    const [isLoading, setIsLoading] = useState(false)
+
+
+
+    function signUpSubmit(e) {
+        e.preventDefault()
+
+        const request = axios.post(`${process.env.REACT_APP_API_URL}/cadastro`, user)
+
+        setIsLoading(true)
+
+        request.then(e => {
+            console.log(e.data)
+            toast.success("Usuário Cadastrado com Sucesso!", {
+                position: "top-center",
+                autoClose: 2000,
+                hideProgressBar: false,
+                closeOnClick: true,
+                pauseOnHover: true,
+                draggable: true,
+                progress: undefined,
+                theme: "dark",
+            })
+            navigate("/")
+            setIsLoading(false)
+        })
+
+        request.catch(e => {
+            const error = e.response.status
+            if (error === 409) {
+                toast.error("Usuário já cadastrado!", {
+                    position: "top-center",
+                    autoClose: 2000,
+                    hideProgressBar: false,
+                    closeOnClick: true,
+                    pauseOnHover: true,
+                    draggable: true,
+                    progress: undefined,
+                    theme: "dark",
+                })
+            }
+            
+            else {
+                toast.error("Erro ao cadastras usuário", {
+                    position: "top-center",
+                    autoClose: 2000,
+                    hideProgressBar: false,
+                    closeOnClick: true,
+                    pauseOnHover: true,
+                    draggable: true,
+                    progress: undefined,
+                    theme: "dark",
+                })
+            }
+
+            console.log(error)
+            setIsLoading(false)
+        })
+    }
+
+
+
     return (
         <RegisterWrapper>
             <h1>
                 MyWallet
             </h1>
-            <StyledForm>
-                <input type="text" placeholder="Nome..."/>
-                <input type="email" placeholder="E-mail.." />
-                <input type="password" placeholder="Senha..."/>
-                <input type="password" placeholder="Confirme a senha..."/>
+            <StyledForm onSubmit={signUpSubmit}>
+                <input
+                    type="text"
+                    placeholder="Nome..."
+                    value={user.name}
+                    onChange={e => setUser({...user, name: e.target.value})}
+                    disabled={isLoading}
+                />
+                <input
+                    type="email"
+                    placeholder="E-mail.."
+                    value={user.email}
+                    onChange={e => setUser({...user, email: e.target.value})}
+                    disabled={isLoading}
+                />
+                <input
+                    type="password"
+                    placeholder="Senha..."
+                    value={user.password}
+                    onChange={e => setUser({...user, password: e.target.value})}
+                    disabled={isLoading}
+                />
+                <input
+                    type="password"
+                    placeholder="Confirme a senha..."
+                    value={user.confirmPassword}
+                    onChange={e => setUser({...user, confirmPassword: e.target.value})}
+                />
                 <StyledButton>
                     Cadastrar
                 </StyledButton>
             </StyledForm>
-            <p>
-                Já tem uma conta? Entre agora!
-            </p>
+
+            <Link to="/">
+                <p>
+                    Já tem uma conta? Entre agora!
+                </p>
+            </Link>
         </RegisterWrapper>
     )
 }
